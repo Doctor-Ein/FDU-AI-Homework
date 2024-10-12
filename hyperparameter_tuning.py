41004f4200cf6ecb6cc2b11dfc23a3a19b68b855
xlx <xlx@sjtu.edu.cn>
2024-10-12 10:12:50 +0800

    SleepingSet and more TODOs Signed-off-by: xlx <xlx@sjtu.edu.cn>
    Signed-off-by: xlx <xlx@sjtu.edu.cn>

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,4 +1,5 @@
 from itertools import product
+from time import sleep
 
 from torch import optim
 
@@ -13,6 +14,7 @@ if __name__ == '__main__':
 	num_epochs = [5, 6, 7]
 	results = []
 
+	i = 0
 	for dropout, lr, momentum, b_size, num_epoch in product(dropouts, learning_rates, momentums, batch_sizes,
 	                                                        num_epochs):
 		print(
@@ -22,7 +24,9 @@ if __name__ == '__main__':
 		train(trainloader, dropout_net, num_epoch, criterion, optimizer, save_path='TrainDataCollection')
 		accuracy = predict(testloader, dropout_net)
 		results.append((dropout, lr, momentum, b_size, num_epoch, accuracy))
-	# TODO：实际跑程序时需要注意sleep休息，同时记得监控计算机相关硬件占用信息，确保程序和设备安全
+		i = i + 1
+		print("sleeping for 60 seconds, num_params:", i)
+		sleep(60)  # 请让电脑休息一分钟，也请记得监控计算机资源占情况，确保程序和设备安全
 
 	# 排序并输出结果
 	results.sort(key=lambda x: x[-1], reverse=True)
@@ -30,3 +34,4 @@ if __name__ == '__main__':
 		print(
 			f'Dropout: {result[0]}, LR: {result[1]}, Momentum: {result[2]}, Batch Size: {result[3]},Num Epochs:{result[4]} ,Accuracy: {result[5]}%')
 # TODO：最佳超参数组合的自动写入
+# TODO：多线程自动搜索
