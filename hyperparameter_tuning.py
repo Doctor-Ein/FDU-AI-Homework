7f5f9196d824fd5e6413400a919f86bd4307679b
xlx <xlx@sjtu.edu.cn>
2024-10-12 10:40:47 +0800

    Better ouputs for observing Signed-off-by: xlx <xlx@sjtu.edu.cn>
    Signed-off-by: xlx <xlx@sjtu.edu.cn>

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -24,6 +24,9 @@ if __name__ == '__main__':
 		train(trainloader, dropout_net, num_epoch, criterion, optimizer, save_path='TrainDataCollection')
 		accuracy = predict(testloader, dropout_net)
 		results.append((dropout, lr, momentum, b_size, num_epoch, accuracy))
+		result = results[-1]
+		print(
+			f'Dropout: {result[0]}, LR: {result[1]}, Momentum: {result[2]}, Batch Size: {result[3]},Num Epochs:{result[4]} ,Accuracy: {result[5]:.1f}%')
 		i = i + 1
 		print("sleeping for 60 seconds, num_params:", i)
 		sleep(60)  # 请让电脑休息一分钟，也请记得监控计算机资源占情况，确保程序和设备安全
@@ -32,6 +35,6 @@ if __name__ == '__main__':
 	results.sort(key=lambda x: x[-1], reverse=True)
 	for result in results:
 		print(
-			f'Dropout: {result[0]}, LR: {result[1]}, Momentum: {result[2]}, Batch Size: {result[3]},Num Epochs:{result[4]} ,Accuracy: {result[5]}%')
+			f'Dropout: {result[0]}, LR: {result[1]}, Momentum: {result[2]}, Batch Size: {result[3]},Num Epochs:{result[4]} ,Accuracy: {result[5]:.1f}%')
 # TODO：最佳超参数组合的自动写入
 # TODO：多线程自动搜索
