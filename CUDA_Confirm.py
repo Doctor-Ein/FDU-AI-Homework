d7e93b6145d53cc170d024a0557081c58fac219e
xlx <xlx@sjtu.edu.cn>
2024-10-13 15:08:41 +0800

    (GPU version)Better_Confirm
    Signed-off-by: xlx <xlx@sjtu.edu.cn>

--- CUDA_Confirm.py
+++ CUDA_Confirm.py
@@ -1,3 +1,6 @@
 import torch
 
-print(torch.cuda.is_available())
+print("CUDA available: ", torch.cuda.is_available())
+print("cuDNN version: ", torch.backends.cudnn.version())
+print("Available GPUs: ", torch.cuda.device_count())
+
